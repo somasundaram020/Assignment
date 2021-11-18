8660ab9a74a21311480519078b80f97faf49ec2d
Somasundaram R.S <68845512+somasundaram020@users.noreply.github.com>
2021-11-18 23:54:03 +0530

    Add files via upload

---
+++ Computer.java
@@ -0,0 +1,10 @@
+package org.system;
+
+public class Computer {
+	
+	public void computerModel() {
+		// TODO Auto-generated method stub
+       System.out.println("computerModel");
+	}
+
+}
---
+++ Desktop.java
@@ -0,0 +1,17 @@
+package org.system;
+
+public class Desktop extends Computer {
+	
+	public void desktopSize() {
+		// TODO Auto-generated method stub
+        System.out.println("desktopSize");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Desktop des= new Desktop();
+       des.computerModel();
+       des.desktopSize();
+	}
+
+}
