644ad29563ffe2fe0ba4297fb63ebc5bc87e25db
Somasundaram R.S <68845512+somasundaram020@users.noreply.github.com>
2021-11-18 23:48:12 +0530

    Add files via upload
    You have to override the method deposit in AxisBank.

---
+++ AxisBank.java
@@ -0,0 +1,16 @@
+package org.bank;
+
+public class AxisBank extends BankInfo{
+	
+	public void deposit() {
+		// TODO Auto-generated method stub
+		System.out.println("deposit by AxisBank");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AxisBank ab=new AxisBank();
+		ab.deposit();
+	}
+
+}
---
+++ BankInfo.java
@@ -0,0 +1,19 @@
+package org.bank;
+
+public class BankInfo {
+	public void saving() {
+		// TODO Auto-generated method stub
+		System.out.println("saving");
+	}
+	
+	public void fixed() {
+		// TODO Auto-generated method stub
+		System.out.println("fixed");
+	}
+	
+	public void deposit() {
+		// TODO Auto-generated method stub
+		System.out.println("deposit");
+	}
+
+}
