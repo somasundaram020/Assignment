8a45bd37da782b1cd51f9b16f40e89060570c589
Somasundaram R.S <68845512+somasundaram020@users.noreply.github.com>
2021-11-19 00:01:38 +0530

    string output

---
+++ JustifyStringOutput.java
@@ -0,0 +1,19 @@
+package assignment;
+
+public class JustifyStringOutput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 String str3 = new String("Kutty");
+		 String str4 = new String("Kutty");
+		 
+		 if(str3==str4) {
+			 System.out.println(" Same text");
+		 }
+		 else
+			 System.out.println("Diff text");
+		 
+		 System.out.println("***");
+	}
+
+}
