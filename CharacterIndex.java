cf9b24bb9ae13e535baaf3cc6da1f966fe72c48a
Somasundaram R.S <68845512+somasundaram020@users.noreply.github.com>
2021-11-19 00:35:15 +0530

    Java program to get the character at the given index within the String.

---
+++ CharacterIndex.java
@@ -0,0 +1,17 @@
+package string;
+
+public class CharacterIndex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String text = "Java Exercise";
+		
+		/*System.out.println(text.charAt(5));
+		System.out.println(text.charAt(11));*/
+		
+		System.out.println("Index for character E "+text.indexOf("E"));
+		System.out.println("Index for character s "+text.indexOf("s"));
+		
+	}
+
+}
