ca332fd7c4c49f5d39e6b1eaad513f043af0af2a
Somasundaram R.S <68845512+somasundaram020@users.noreply.github.com>
2021-11-19 01:12:52 +0530

    replace a specified character with another character

---
+++ ReplaceCharacter.java
@@ -0,0 +1,14 @@
+package string;
+
+public class ReplaceCharacter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String sentence = "I am working with Java8";
+		System.out.println(sentence.replace("8","10"));
+		System.out.println(sentence.substring(5, 14));
+		
+		//A part of String is called substring.
+	}
+
+}
